831ccee8ddcab2f805871f2f0979825054ab0ee3
Alexgan063 <ganeev.aleksandr@gmail.com>
2023-09-20 23:09:48 +0300

    Решил задания в разделе 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,46 @@
-function Student(name, gender, age) {
+function Student(name, gender, age){
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
   
-}
+  }
 
 Student.prototype.setSubject = function (subjectName) {
   
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function(...marks) {
+    if (marks.length === undefined || marks.length === 0) {
+        return 0;
+    }
+
+    if (Object.keys(this).includes('excludeds')){
+        return 0;
+    }
+
+    if (Object.keys(this).includes('marks')) {
+      this.marks.push(...marks);
+    } 
+  }
 
 Student.prototype.getAverage = function () {
+    
+    if (this.marks.length === 0) {
+        return 0;
+    }
   
+  let sum = this.marks.reduce((acc, number) => acc + number , 0);
+ 
+  return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
+
+    delete this.marks;
+    delete this.subject;
+
+    this.excluded = reason;
   
 }
