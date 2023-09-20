7127b37907855a2f8dda363ae55c66d2d07c8a56
Alexgan063 <ganeev.aleksandr@gmail.com>
2023-09-20 20:55:29 +0300

    Указал комментарии - проверка ветки

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,17 @@
 function compareArrays(arr1, arr2) {
+
   return arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    // проверяем задали массив входящий или нет
     if (isNaN(users.length) || users.length === 0 ) {
         return 0;
     }
     
     let result = users.filter((user) => user.gender === gender).map((user) => user.age);
     
+    // проверяем полученный массив значений
     if (isNaN (result.length) || result.length === 0) {
         return 0;
     }
