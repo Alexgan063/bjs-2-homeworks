1cf688b523b3b215bd0ad1d234b2cb030ca71e53
Alexgan063 <ganeev.aleksandr@gmail.com>
2023-09-20 20:39:37 +0300

    Задания с массивами 3.arrays сделано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    if (isNaN(users.length) || users.length === 0 ) {
+        return 0;
+    }
+    
+    let result = users.filter((user) => user.gender === gender).map((user) => user.age);
+    
+    if (isNaN (result.length) || result.length === 0) {
+        return 0;
+    }
+    
+    return result.reduce((acc, age) => acc + age, 0) / result.length;
+  }
   
-}
+  
