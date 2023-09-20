84d71f9c286e13d90fbfe5c458d9933672d24f6f
Alexgan063 <ganeev.aleksandr@gmail.com>
2023-09-20 23:18:35 +0300

    Добавил комментарии в разделе 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -15,6 +15,7 @@ Student.prototype.addMarks = function(...marks) {
     if (marks.length === undefined || marks.length === 0) {
         return 0;
     }
+// Проверки на то, есть ли свойство или нет
 
     if (Object.keys(this).includes('excludeds')){
         return 0;
